95e234148fbe667ef4d376e9e4e7b5db2a10614b
Antonia Langer <ai21m032@technikum-wien.at>
2022-12-01 20:50:43 +0100

    Deleted Test-Folder, added Dropdown-Selection Parameters to API

--- swagger_server/controllers/libretranslate_get_controller.py
+++ swagger_server/controllers/libretranslate_get_controller.py
@@ -6,20 +6,24 @@ import json
 from swagger_server import util
 
 
-def language_sloth_rest_libretranslate_get(text):  # noqa: E501
+def language_sloth_rest_libretranslate_get(text, source_language, target_language):  # noqa: E501
     """language_sloth_rest_libretranslate_get
 
-    Retrieves a file from the filesystem # noqa: E501
+    Retrieves the translation for a given text # noqa: E501
 
     :param text: text to translate
     :type text: str
+    :param source_language: source language of the text
+    :type source_language: str
+    :param target_language: target language of the text
+    :type target_language: str
 
     :rtype: str
     """
 
     #Call the translation
     url = 'http://192.168.0.228:5000/translate'
-    params = {'q': text, 'source': 'en', 'target': 'es', 'format':'text'}
+    params = {'q': text, 'source': source_language, 'target': target_language, 'format':'text'}
 
     jsonResponse =  requests.post(url=url, data=params)
     stringResponse = json.loads(jsonResponse.content)
