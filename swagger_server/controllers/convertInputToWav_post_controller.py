68fe6d78ff00effa5b2ef961418e1061173ebc50
Antonia Langer <ai21m032@technikum-wien.at>
2023-01-10 18:53:27 +0100

    Added file sending

--- swagger_server/controllers/convertInputToWav_post_controller.py
+++ swagger_server/controllers/convertInputToWav_post_controller.py
@@ -6,14 +6,10 @@ import requests
 import json
 import flask
 
-from werkzeug.utils import secure_filename
-from werkzeug.datastructures import FileStorage
 from swagger_server import util
-from pydub import AudioSegment
 from io import BytesIO
-import base64
 
-from flask import Flask, request
+from flask import Flask, request, send_file, make_response
 
 UPLOAD_FOLDER = './files'
 
@@ -24,68 +20,35 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def language_sloth_rest_convertInputToWav_post(file=None):  # noqa: E501
     """language_sloth_rest_convertInputToWav_post
 
-    Retrieves the translation for a given text # noqa: E501
+    Retrieves input recording, translates it to given language and returns output recording.
 
     :rtype: str
     """
-    
-    print(app)
-
 
     if 'file' not in request.files:
-        return {"status": "No file part"}, 409
+        return {"status": "No file included!"}, 409
 
     file = request.files['file']
 
     if file.filename == '':
-        return {"status": "No selected file"}, 404
+        return {"status": "No file selected!"}, 404
 
     if file:
-        #filename = secure_filename(file.filename)
-
-        with open(os.path.join(UPLOAD_FOLDER, "test.webm"), "wb") as fp:
+        with open(os.path.join(UPLOAD_FOLDER, "recording.wav"), "wb") as fp:
             fp.write(file.read())
         
-
-
-        #filename = secure_filename(file.filename)
-
-        #file.open("test.webm", "wb")
-        #ad()
-        
-        #file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'blob.wav'))
-
-        # with open("file.webm", "wb") as f:
-        #     file_stream = file.read()
-        #     print(f)
-        #     file.write(file_stream)
-        #     print(f)
-        #     file.save(os.path.join(app.config['UPLOAD_FOLDER'], f.name))
+        # TODO Send WAV to CoquiSST
+        # TODO Send received text to LibreTranslate
+        # TODO Send translated text to CoquiTTS
+        # TODO Return output audio to frontend
+        path_test = "/usr/src/app/files/recording.wav"
         
         
-        #filename = secure_filename(file.filename)
+        
+        return send_file(
+         path_test, 
+         mimetype="audio/wav", 
+         as_attachment=True, 
+         attachment_filename="translatedRecording.wav")
 
-
-
-        #file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-
-    return ('hello')
-    
-    
-    #print(file)
-    #fileRead = file.read()
-    #print(fileRead)
-
-    #f = open('test.wav', 'wb')
-    #f.write(fileRead)
-    #f.close()
-
-
-    #print(f)
-    #print(fileRead.size)
-    #print(fileRead.type)
-
-    #given_audio = AudioSegment.from_file(f, format="webm")
-
-    #Call the translation   
-    return 'request.form'
+    return ('No file found!')
